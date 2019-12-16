cae87f4b3209c710853e3fc60753553457047808
gwayne9955 <gwayne9955@yahoo.com>
2019-12-16 11:37:24 -0800

    after db migrate

---
+++ Migrations/20191216193505_StartOfMovieAPI2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace movie_project.Migrations
+{
+    public partial class StartOfMovieAPI2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Movies_imdbID",
+                table: "Movies");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Movies_imdbID",
+                table: "Movies",
+                column: "imdbID");
+        }
+    }
+}
