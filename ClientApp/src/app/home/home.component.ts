5d153d1315949d05c5ee76810e36397586c5d844
gwayne9955 <gwayne9955@yahoo.com>
2019-12-16 23:43:24 -0800

    Scroll to the bottom to load more movies

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, HostListener } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -9,13 +9,20 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HomeComponent {
   private tmdbResponse: TMDBResponse;
+  private tmdbMovies;
   private omdbResponse: MovieSearchResult;
   private newArray;
+  private pageNum: number;
+  private columns: number;
 
   constructor(private http: HttpClient, 
     @Inject('BASE_URL') private baseUrl: string, 
   private route: ActivatedRoute, 
   private router: Router) {
+    this.newArray = [];
+    this.pageNum = 1;
+    this.columns = 3;
+    this.tmdbMovies = [];
     this.getPopularMovies();
   }
 
@@ -23,15 +30,15 @@ export class HomeComponent {
     this.http.get<TMDBResponse>("https://api.themoviedb.org/3/discover/movie", {
         params: {
           api_key: "f28df3fec9ce98f371cc2a6636044a45",
-          sort_by: "popularity.desc"
+          sort_by: "popularity.desc",
+          page: this.pageNum.toString()
         }})
       .subscribe(result => {
         this.tmdbResponse = result;
-        var k = 3;
+        this.tmdbMovies = this.tmdbMovies.concat(this.tmdbResponse.results);
         this.newArray = [];
-        for (let i = 0; i < this.tmdbResponse.results.length; i += k) {
-
-          this.newArray.push({ items: this.tmdbResponse.results.slice(i, i + k) });
+        for (let i = 0; i < this.tmdbMovies.length; i += this.columns) {
+          this.newArray.push({ items: this.tmdbMovies.slice(i, i + this.columns) });
         }
       }, error => console.error(error));
   }
@@ -46,6 +53,19 @@ export class HomeComponent {
         this.omdbResponse = result;
         this.router.navigateByUrl(`/movie/${this.omdbResponse.Search[0].imdbID}`);
       }, error => console.error(error));
+  }
+
+  
+
+  @HostListener("window:scroll", ["$event"])
+  onWindowScroll() {
+    //In chrome and some browser scroll is given to body tag
+    let pos = (document.documentElement.scrollHeight || document.body.scrollHeight) - document.documentElement.clientHeight;
+    let max = document.documentElement.scrollTop;
+    if (pos == max)   {
+      this.pageNum++;
+      this.getPopularMovies();
+    }
   }
 }
 
