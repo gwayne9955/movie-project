8c883e293ea81604b0dec172b893e6edbf0accbf
gwayne9955 <gwayne9955@yahoo.com>
2019-12-19 21:31:00 -0800

    after migration, with edits to the sql migration

---
+++ Migrations/20191220052834_AddUniqueMovieListUserIdAndName.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace movie_project.Migrations
+{
+    public partial class AddUniqueMovieListUserIdAndName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.DropIndex(
+            //    name: "IX_MovieLists_ApplicationUserRefId",
+            //    table: "MovieLists");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieLists_ApplicationUserRefId_Name",
+                table: "MovieLists",
+                columns: new[] { "ApplicationUserRefId", "Name" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_MovieLists_ApplicationUserRefId_Name",
+                table: "MovieLists");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieLists_ApplicationUserRefId",
+                table: "MovieLists",
+                column: "ApplicationUserRefId");
+        }
+    }
+}
